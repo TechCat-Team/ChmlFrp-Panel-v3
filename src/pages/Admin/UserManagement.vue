d2137b5297e576dab363f8fb8b989d0bb0f30d93
2010tcsy <github@ighx.me>
2024-08-11 15:05:03 +0800

    第三次重写,页面优化

--- src/pages/Admin/UserManagement.vue
+++ src/pages/Admin/UserManagement.vue
@@ -1,180 +1,188 @@
 <template>
-  <div class="container">
     <n-back-top :right="100" />
-    <n-card size="small" class="top-card">
-      <n-collapse>
-        <n-collapse-item name="1" title="添加用户" class="custom-collapse-item">
-          <n-card size="small">
-            <template #header-extra>
-              <n-tag round :bordered="false" type="primary">
-                用户表单
-              </n-tag>
-            </template>
-            <n-form :model="userForm" label-width="100">
-              <n-form-item label="用户名">
-                <n-input v-model="userForm.username" placeholder="输入用户名" />
-              </n-form-item>
-              <n-form-item label="邮箱">
-                <n-input v-model="userForm.email" placeholder="输入邮箱" />
-              </n-form-item>
-              <n-form-item label="密码">
-                <n-input type="password" v-model="userForm.password" placeholder="输入密码" />
-              </n-form-item>
-              <n-form-item>
-                <n-button type="primary" @click="addUser">添加用户</n-button>
-              </n-form-item>
-            </n-form>
-          </n-card>
-        </n-collapse-item>
-
-        <n-collapse-item name="2" title="删除用户" class="custom-collapse-item">
-          <n-card size="small">
-            <template #header-extra>
-              <n-tag round :bordered="false" type="primary">
-                用户删除
-              </n-tag>
-            </template>
-            <n-form :model="userForm" label-width="100">
-              <n-form-item label="用户名">
-                <n-input v-model="userForm.username" placeholder="输入用户名" />
-              </n-form-item>
-              <n-form-item label="邮箱">
-                <n-input v-model="userForm.email" placeholder="输入邮箱" />
-              </n-form-item>
-              <n-form-item>
-                <n-button type="primary" @click="delUser">删除</n-button>
-              </n-form-item>
-            </n-form>
-          </n-card>
-        </n-collapse-item>
-
-        <n-collapse-item name="3" title="封禁用户" class="custom-collapse-item">
-          <n-card size="small">
-            <template #header-extra>
-              <n-tag round :bordered="false" type="primary">
-                用户封禁
-              </n-tag>
-            </template>
-            <n-form :model="userForm" label-width="100">
-              <n-form-item label="用户名">
-                <n-input v-model="userForm.username" placeholder="输入用户名" />
-              </n-form-item>
-              <n-form-item label="邮箱">
-                <n-input v-model="userForm.email" placeholder="输入邮箱" />
-              </n-form-item>
-              <n-form-item label="封禁原因">
-                <n-input v-model="userForm.why" placeholder="输入封禁原因" />
-              </n-form-item>
-              <n-form-item label="解封时间">
-                <n-input type="date" v-model="userForm.time" placeholder="" />
-              </n-form-item>
-              <n-form-item>
-                <n-button type="primary" @click="banUser">GoBan</n-button>
-              </n-form-item>
-            </n-form>
-          </n-card>
-        </n-collapse-item>
-
-        <n-collapse-item name="4" title="解封用户" class="custom-collapse-item">
-          <n-card size="small">
-            <template #header-extra>
-              <n-tag round :bordered="false" type="primary">
-                用户解封
-              </n-tag>
-            </template>
-            <n-form :model="userForm" label-width="100">
-              <n-form-item label="用户名">
-                <n-input v-model="userForm.username" placeholder="输入用户名" />
-              </n-form-item>
-              <n-form-item label="邮箱">
-                <n-input v-model="userForm.email" placeholder="输入邮箱" />
-              </n-form-item>
-              <n-form-item>
-                <n-button type="primary" @click="unBanUser">UnBan</n-button>
-              </n-form-item>
-            </n-form>
-          </n-card>
-        </n-collapse-item>
-
-        <n-collapse-item name="5" title="给予会员" class="custom-collapse-item">
-          <n-card size="small">
-            <template #header-extra>
-              <n-tag round :bordered="false" type="primary">
-                会员管理
-              </n-tag>
-            </template>
-            <n-form :model="membershipForm" label-width="100">
-              <n-form-item label="用户名">
-                <n-input v-model="membershipForm.username" placeholder="输入用户名" />
-              </n-form-item>
-              <n-form-item label="邮箱">
-                <n-input v-model="membershipForm.email" placeholder="输入邮箱" />
-              </n-form-item>
-              <n-form-item label="会员期限">
-                <n-input type="date" v-model="membershipForm.expiryDate" placeholder="" />
-              </n-form-item>
-              <n-form-item>
-                <n-button type="primary" @click="manageMembership">提交</n-button>
-              </n-form-item>
-            </n-form>
-          </n-card>
-        </n-collapse-item>
-      </n-collapse>
+    <n-card title="用户管理">
+        <template #header-extra>
+          <div class="header-extra-content">
+            <input type="text" placeholder="搜索..." class="search-input" />
+            <n-button class="search-button" round strong secondary size="small">搜索</n-button>
+            <n-button class="add-user-button" round type="primary">添加用户</n-button>
+          </div>
+        </template>
+        <n-grid cols="2 m:5 xl:7" responsive="screen" v-if="!available">
+            <n-grid-item>
+                <n-statistic label="当前总用户" tabular-nums>
+                    <n-number-animation :from="0" show-separator :to="114514" />
+                </n-statistic>
+            </n-grid-item>
+            <n-grid-item>
+                <n-statistic label="当前总VIP" tabular-nums>
+                    <n-number-animation :from="0" show-separator :to="1919" />
+                </n-statistic>
+            </n-grid-item>
+            <n-grid-item span="2 s:1">
+                <n-statistic label="当前总封禁用户" tabular-nums>
+                    <n-number-animation :from="0" show-separator :to="3" />
+                </n-statistic>
+            </n-grid-item>
+            <n-grid-item span="2 s:1">
+                <n-statistic label="用户总消费" tabular-nums>
+                    <n-number-animation :from="0" :to="421.26" :precision="2" />
+                    <template #suffix> RMB </template>
+                </n-statistic>
+            </n-grid-item>
+            <n-grid-item span="2 s:1">
+                <n-statistic label="数据更新时间" tabular-nums>
+                    2024-11-04 51:41
+                </n-statistic>
+            </n-grid-item>
+        </n-grid>
     </n-card>
-  </div>
+    <n-grid v-if="!available" cols="1 m:2 l:3 xl:4 2xl:5" style="margin-top: 20px;" :x-gap="12" :y-gap="12" responsive="screen">
+        <n-grid-item v-for="(userStatusCard, index) in userStatusCards" :key="index">
+            <n-card size="small" hoverable @click="goToNodeInfo">
+                <template #header>
+                    {{ userStatusCard.title }} <span style="color: gray; font-size: 14px;">{{ userStatusCard.id }}</span>
+                </template>
+                <template #header-extra>
+                    <n-flex>
+                        <n-tag round :bordered="false" :style="tagStyle(userStatusCard.state)" v-if="userStatusCard.state !== undefined">
+                            {{ tagText(userStatusCard.state) }}
+                        </n-tag>
+                    </n-flex>
+                </template>
+                <n-flex justify="space-around" size="large" align="center">
+                    <n-descriptions label-placement="left" :column="1">
+                        <n-descriptions-item label="封禁时间">{{ userStatusCard.bannedTime }}</n-descriptions-item>
+                        <n-descriptions-item label="原因">{{ userStatusCard.whyBanned }}</n-descriptions-item>
+                        <n-descriptions-item label="解封时间">{{ userStatusCard.unBannedTime }}</n-descriptions-item>
+                    </n-descriptions>
+                </n-flex>
+            </n-card>
+        </n-grid-item>
+    </n-grid>
+    <n-grid v-else :cols="1" style="margin-top: 20px;" :x-gap="12" :y-gap="12" responsive="screen">
+        <n-grid-item>
+            <n-card hoverable @click="goToNodeInfo">
+                <ServiceUptime />
+            </n-card>
+        </n-grid-item>
+        <n-grid-item>
+            <n-card hoverable @click="goToNodeInfo">
+                <ServiceUptime />
+            </n-card>
+        </n-grid-item>
+        <n-grid-item>
+            <n-card hoverable @click="goToNodeInfo">
+                <ServiceUptime />
+            </n-card>
+        </n-grid-item>
+    </n-grid>
 </template>
 
 <script lang="ts" setup>
 import { ref } from 'vue';
+import { useRouter } from 'vue-router';
 
-const userForm = ref({
-  username: '',
-  email: '',
-  password: '',
-  why: '',
-  time: '',
-});
+const available = ref(false);
+const router = useRouter();
 
-const membershipForm = ref({
-  username: '',
-  email: '',
-  expiryDate: '',
-});
-
-function addUser() {
-  // 添加用户的逻辑
+const goToNodeInfo = () => {
+//    const url = router.resolve({ path: '/node/iusers' }).href;
+//    window.open(url, '_blank');
 }
 
-function delUser() {
-  // 删除用户的逻辑
-}
+const userStatusCards = ref([
+    {
+        id: '#3',
+        title: 'chaoji',
+        group: 'user',
+        bannedTime: '公元前249年',
+        whyBanned: '于2077年黑入荒板塔，袭击了一名网络安全人员。',
+        unBannedTime: '公元前210年',
+        state: true,
+        imgUrl: "https://chmlfrp.cn/favicon.ico"
+    },
+    {
+        id: '#4',
+        title: '24****494',
+        group: 'user',
+        bannedTime: '2024-6-27 22:20',
+        whyBanned: '在上周的疯狂星期四没有V我50',
+        unBannedTime: '2024-7-4 0:00',
+        state: false,
+        imgUrl: "https://chmlfrp.cn/favicon.ico"
+    },
+    {
+        id: '#12',
+        title: '133****1926',
+        group: 'user',
+        bannedTime: '2044-3-12 35:92',
+        whyBanned: '因为没有被封禁，所以被封禁。',
+        unBannedTime: '永久封禁',
+        state: undefined,
+        imgUrl: "https://chmlfrp.cn/favicon.ico"
+    },
+]);
 
-function banUser() {
-  // 封禁用户的逻辑
-}
 
-function unBanUser() {
-  // 解封用户的逻辑
-}
+const tagText = (state) => {
+    if (state === true) {
+        return '解封';
+    } else if (state === false) {
+        return '封禁';
+    } else {
+        return '未知';
+    }
+};
 
-function manageMembership() {
-  // 给予/收回会员的逻辑
-}
+const tagStyle = (state) => {
+    if (state === true) {
+        return { color: 'green', backgroundColor: 'lightgreen' };
+    } else if (state === false) {
+        return { color: 'yellow', backgroundColor: 'lightyellow' };
+    } else {
+        return { color: 'gray', backgroundColor: 'lightgray' };
+    }
+};
 </script>
 
 <style scoped>
-.container {
-  padding: 20px;
-  background-color: #f5f5f5;
+.header-extra-content {
+  display: flex;
+  align-items: center;
 }
 
-.top-card {
-  padding: 20px;
-  background-color: #ffffff; /* 胶带顶级卡片的背景颜色 */
+.search-input {
+  padding: 8px;
+  border: 2px solid #ddd;
+  border-radius: 4px;
+  margin-right: 8px;
+  transition: width 0.3s ease;
+  width: 200px;
 }
 
-.custom-collapse-item .n-collapse-item-content {
-  background-color: #ffffff; /* 设置折叠内容的背景颜色为白色 */
-  padding: 16px;
+.search-input:focus {
+  width: 300px;
+  border-color: #007bff;
+}
+
+.search-button,
+.add-user-button {
+  padding: 8px 16px;
+  border: none;
+  border-radius: 4px;
+  margin-left: 8px;
+  cursor: pointer;
+  font-size: 14px;
+  color: white;
+  transition: background-color 0.3s ease;
+}
+
+.search-button {
+  background-color: #EEEEEE;
+}
+
+.search-button:hover {
+  background-color: #BBBBBB; 
 }
 </style>
